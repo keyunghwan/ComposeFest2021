455bc64602ada9c9647233eaf9c9b833d187e0b6
keyunghwan <keyunghwan@gmail.com>
2021-11-05 10:32:37 +0900

    7 state 할 차례

--- oneweekcode/app/src/main/java/com/example/oneweekcode1/MainActivity.kt
+++ oneweekcode/app/src/main/java/com/example/oneweekcode1/MainActivity.kt
@@ -3,11 +3,14 @@ package com.example.oneweekcode1
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.oneweekcode1.ui.theme.Oneweekcode1Theme
 
 class MainActivity : ComponentActivity() {
@@ -15,24 +18,41 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             Oneweekcode1Theme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
+                MyApp()
             }
         }
     }
 }
 
+
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+private fun MyApp() {
+    Surface(color = MaterialTheme.colors.primary) {
+        Greeting("Keyunghwan oh")
+    }
+}
+
+@Composable
+private fun Greeting(name: String) {
+    Surface(
+        color = MaterialTheme.colors.primary,
+        modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+    ) {
+        Row(modifier = Modifier.padding(24.dp)) {
+        Column(modifier = Modifier.weight(1f)) {
+            Text(text = "Hello,")
+            Text(text = name)
+        }
+           OutlinedButton(onClick = { /* TODO */}
+            ) { Text("Show more")}
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
+private fun DefaultPreview() {
     Oneweekcode1Theme {
-        Greeting("Android")
+        MyApp()
     }
 }
