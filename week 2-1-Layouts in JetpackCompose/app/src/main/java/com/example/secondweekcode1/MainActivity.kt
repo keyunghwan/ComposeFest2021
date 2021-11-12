a01f856f750598c4e1a53dcb2ad8ffe90744d408
keyunghwan <keyunghwan@gmail.com>
2021-11-12 18:36:50 +0900

    layoutcodelab 끝냄

--- week 2-1-Layouts in JetpackCompose/app/src/main/java/com/example/secondweekcode1/MainActivity.kt
+++ week 2-1-Layouts in JetpackCompose/app/src/main/java/com/example/secondweekcode1/MainActivity.kt
@@ -3,13 +3,16 @@ package com.example.secondweekcode1
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.CompositionLocalProvider
-import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.secondweekcode1.ui.theme.LayoutsCodelabTheme
+
+
+
 
 
 
@@ -18,12 +21,11 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             LayoutsCodelabTheme {
-                  LayoutsCodelab()
+                LargeConstraintLayout()
                 }
             }
         }
     }
-
 
 
 
